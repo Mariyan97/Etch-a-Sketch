a66ecc607600c2bd73854e845bbaf209a23fc20c
Mariyan97 <webdev.mvmnt@gmail.com>
2023-10-04 20:12:24 +0300

    Added the last 2 buttons that i need for that project

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const clearButton = document.getElementById("clear-button");
 const slider = document.getElementById("slider");
 const colorPicker = document.getElementById("color-picker");
 const gridInfo = document.getElementById("grid-info");
+const colorModeButton = document.getElementById("color-mode-button");
+const eraserButton = document.getElementById("eraser-button");
+
 let currentColor = colorPicker.value;
+let isColorMode = true;
 
 function createGrid(rows, cols) {
-  container.innerHTML = "";
-
+  clearGrid();
   container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-
   for (let i = 0; i < rows * cols; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     container.appendChild(cell);
-
     cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = currentColor;
+      if (isColorMode) {
+        cell.style.backgroundColor = currentColor;
+      } else {
+        cell.style.backgroundColor = "white";
+      }
     });
   }
+
   gridInfo.textContent = `${rows}x${cols}`;
 }
 
@@ -43,4 +49,10 @@ colorPicker.addEventListener("input", () => {
   currentColor = colorPicker.value;
 });
 
-displayColums.textContent = gridSize;
+colorModeButton.addEventListener("click", () => {
+  isColorMode = true;
+});
+
+eraserButton.addEventListener("click", () => {
+  isColorMode = false;
+});
